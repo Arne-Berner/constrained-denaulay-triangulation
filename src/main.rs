97dc40bb2c6f1d02ef2a187d5099038c2b600ad0
Arne Berner <arne.berner@student.fh-kiel.de>
2023-09-15 16:28:23 +0200

    added some gizmo lines to test things

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,43 @@
-use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, (system, update_config))
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
-) {
-    // Load the Bevy logo as a texture
-    let texture_handle = asset_server.load("branding/banner.png");
-    // Build a default quad mesh
-    let mut mesh = Mesh::from(shape::Quad::default());
-    // Build vertex colors for the quad. One entry per vertex (the corners of the quad)
-    let vertex_colors: Vec<[f32; 4]> = vec![
-        Color::RED.as_rgba_f32(),
-        Color::GREEN.as_rgba_f32(),
-        Color::BLUE.as_rgba_f32(),
-        Color::WHITE.as_rgba_f32(),
-    ];
-    // Insert the vertex colors as an attribute
-    mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors);
-
-    let mesh_handle: Mesh2dHandle = meshes.add(mesh).into();
-
-    // Spawn camera
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+    // text
+    commands.spawn(TextBundle::from_section(
+        "Hold 'Left' or 'Right' to change the line width",
+        TextStyle {
+            font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+            font_size: 24.,
+            color: Color::WHITE,
+        },
+    ));
+}
 
-    // Spawn the quad with vertex colors
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: mesh_handle.clone(),
-        transform: Transform::from_translation(Vec3::new(-96., 0., 0.))
-            .with_scale(Vec3::splat(128.)),
-        material: materials.add(ColorMaterial::default()),
-        ..default()
-    });
+fn system(mut gizmos: Gizmos) {
+    gizmos.line_gradient_2d(Vec2::Y , Vec2::splat(-80.), Color::RED, Color::SILVER);
 
-    // Spawning the quad with vertex colors and a texture results in tinting
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: mesh_handle,
-        transform: Transform::from_translation(Vec3::new(96., 0., 0.))
-            .with_scale(Vec3::splat(128.)),
-        material: materials.add(ColorMaterial::from(texture_handle)),
-        ..default()
-    });
+    // Triangle
+    gizmos.linestrip_gradient_2d([
+        (Vec2::Y * 300., Color::BLUE),
+        (Vec2::new(-255., -155.), Color::RED),
+        (Vec2::new(255., -155.), Color::GREEN),
+        (Vec2::Y * 300., Color::YELLOW),
+    ]);
+}
+
+fn update_config(mut config: ResMut<GizmoConfig>, keyboard: Res<Input<KeyCode>>, time: Res<Time>) {
+    if keyboard.pressed(KeyCode::Right) {
+        config.line_width += 5. * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Left) {
+        config.line_width -= 5. * time.delta_seconds();
+    }
 }
