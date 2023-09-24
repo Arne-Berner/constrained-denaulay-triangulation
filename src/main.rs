0a37a411ed237f02e0b81b224685dfa4158cfc29
Arne Berner <arne.berner@student.fh-kiel.de>
2023-09-24 14:02:05 +0200

    no panic, no sadness?

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,30 @@
 use data_structures::vector::Vector;
 
 mod data_structures;
+mod hole_creation;
 mod math_utils;
 mod normalize;
 mod triangulation;
-mod hole_creation;
 
 fn main() {
     let mut input_points = Vec::new();
-    input_points.push(Vector::new(1., 1.));
-    input_points.push(Vector::new(3., 4.));
+    input_points.push(Vector::new(-0., 7.0));
+    input_points.push(Vector::new(-5., 5.));
+    input_points.push(Vector::new(5., 5.));
     input_points.push(Vector::new(-2., 3.));
-    input_points.push(Vector::new(-2., 3.));
-    input_points.push(Vector::new(-2., -2.));
-    input_points.push(Vector::new(-1., -1.));
-    input_points.push(Vector::new(-2., -3.));
-    input_points.push(Vector::new(4., -2.));
-    let (triangle_set, bounds) = match triangulation::triangulate(&mut input_points, None, None) {
+    input_points.push(Vector::new(3., 1.));
+    input_points.push(Vector::new(-4., -1.));
+    input_points.push(Vector::new(1., -2.));
+    input_points.push(Vector::new(-6., -4.));
+    input_points.push(Vector::new(5., -4.));
+
+    let mut holes = vec![Vec::<Vector>::new()];
+    let mut hole = vec![];
+    hole.push(Vector::new(-1., -1.));
+    hole.push(Vector::new(1., -1.));
+    hole.push(Vector::new(0., 1.));
+
+    let (triangle_set, bounds) = match triangulation::triangulate(&mut input_points, Some(&mut holes), None) {
         Ok(result) => result,
         Err(err) => panic!("triangulation failed!{:?}", err),
     };
